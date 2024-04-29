7f1ff9d7981014dcbf979594b880530733557ee2
gauravJ073 <gauravjakhar073@gmail.com>
2024-04-29 13:38:57 +0530

    improved test.cpp

--- test.cpp
+++ test.cpp
@@ -7,18 +7,38 @@
 int main(){
     CSVdata data;
 
+    cout<<"Enter the file name for training data: ";
+    string training_file;
+    cin>>training_file;
+
+    cout<<"Enter the file name for training data: ";
+    string testing_file;
+    cin>>testing_file;
+
+    cout<<"Enter the number of features in data: ";
+    int input_layer;
+    cin>>input_layer;
+    cout<<"Enter the number of output classes in data: ";
+    int output_layer;
+    cin>>output_layer;
+
     vector<unsigned> topology;
-    topology.push_back(32);
-    topology.push_back(16);
-    topology.push_back(16);
-    // topology.push_back(4);
-    // topology.push_back(4);
-    topology.push_back(2);
+    topology.push_back(input_layer);
+    cout<<"Enter the number of layers you want in the neural network: ";
+    int n_layer;
+    cin>>n_layer;
+    for(int i=0;i<n_layer;i++){
+        cout<<"Enter the number of neurons you want in the "<<i+1<<" layer: ";
+        int n_neurons;
+        cin>>n_neurons;
+        topology.push_back(n_neurons);
+    }
+    topology.push_back(output_layer);
 
-    NNModel model(100, 10, topology, ".\\dataset\\ionosphere_train.csv");
+
+    NNModel model(100, 10, topology, ".\\dataset\\"+training_file);
     model.train();
-
-    model.calcConfusionMatrix(model.test(".\\dataset\\ionosphere_test.csv"));
+    model.calcConfusionMatrix(model.test(".\\dataset\\"+testing_file));
     model.printConfusionMatrix();
     model.printAccuracy();
 }
